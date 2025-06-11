afea239ec3dd473cb12aec9e6ca44310466921e8
Agna2503 <solankiagnahardasbhai@gmail.com>
2025-06-11 18:36:39 +0530

    fix: added class of content.

--- src/pages/DashboardEcommerce/index.jsx
+++ src/pages/DashboardEcommerce/index.jsx
@@ -6,23 +6,7 @@ const DashboardEcommerce = () => {
 
   return (
     <React.Fragment>
-      <div className="">
-        <Container fluid>
-          <Row>
-            <Col>
-              <div className="h-100">
-                {/* <Section rightClickBtn={toggleRightColumn} /> */}
-                <Row></Row>
-                <Row>
-                  <Col xl={8}></Col>
-                </Row>
-                <Row></Row>
-                <Row></Row>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <div className="page-content">My Dahsboard</div>
     </React.Fragment>
   );
 };
