cc0773c5635680e45ff60378fc083118b835b19d
Agna2503 <solankiagnahardasbhai@gmail.com>
2025-06-17 15:38:54 +0530

    Footer changes

--- src/Components/BASE/BaseInput.jsx
+++ src/Components/BASE/BaseInput.jsx
@@ -14,7 +14,6 @@ const BaseInput = ({
   maxLength,
   onBlur = () => {},
   required = false,
-  onKeyDown = () => {},
   setPasswordShown = () => {},
 }) => {
   const inputType = showPasswordToggle
@@ -22,6 +21,12 @@ const BaseInput = ({
       ? "text"
       : "password"
     : type;
+  const handleKeyDown = (event) => {
+    const preventTypes = ["email", "password"];
+    if (preventTypes.includes(inputType) && event.code === "Space") {
+      event.preventDefault();
+    }
+  };
 
   return (
     <div>
@@ -41,7 +46,7 @@ const BaseInput = ({
           onBlur={onBlur}
           value={value}
           maxLength={maxLength}
-          onKeyDown={onKeyDown}
+          onKeyDown={handleKeyDown}
         />
         {showPasswordToggle && (
           <InputGroupText onClick={() => setPasswordShown(!passwordShown)}>
--- src/Layouts/Footer.jsx
+++ src/Layouts/Footer.jsx
@@ -1,30 +1,45 @@
-import React from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import { LoginRoutes } from "../Routes/apiRoutes";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <React.Fragment>
-            <footer className="footer">
-                <Container fluid>
-                    <Row>
-                        <Col sm={6}>
-                        <p className="mb-0 text-muted">
-                            © {new Date().getFullYear()} Shivinfotech.
-                            </p>
-                        </Col>
-                        <Col sm={6}>
-                            <div className="text-sm-end d-none d-sm-block">
-                                                      <p className="mb-0 text-muted">
-
-                              Design & Develop by Shiv Infotech.
-                              </p>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </footer>
-        </React.Fragment>
-    );
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname === LoginRoutes.LOGIN ||
+    location.pathname === LoginRoutes.RESET;
+  return (
+    <React.Fragment>
+      <footer className="footer">
+        <Container fluid>
+          {isAuthPage ? (
+            <Row>
+              <Col className="text-center">
+                <p className="mb-0 text-muted">
+                  © {new Date().getFullYear()} Shivinfotech.
+                </p>
+              </Col>
+            </Row>
+          ) : (
+            <Row>
+              <Col sm={6}>
+                <p className="mb-0 text-muted">
+                  © {new Date().getFullYear()} Shivinfotech.
+                </p>
+              </Col>
+              <Col sm={6}>
+                <div className="text-sm-end d-none d-sm-block">
+                  <p className="mb-0 text-muted">
+                    Design & Develop by Shiv Infotech.
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          )}
+        </Container>
+      </footer>
+    </React.Fragment>
+  );
 };
 
 export default Footer;
